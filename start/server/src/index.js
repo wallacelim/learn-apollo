fb46ceeaeec0d8f7a3afc41ffa095e2e45e758b5
Wallace <wallacelim97@gmail.com>
2020-04-23 00:57:29 +0800

    Build Schemas

---
+++ start/server/src/index.js
@@ -0,0 +1,7 @@
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
+
+const server = new ApolloServer({ typeDefs });
+server.listen().then(({ url }) => {
+    console.log(`server ready at ${url}`);
+});
