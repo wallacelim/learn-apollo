91dbbefc24e62d2233ef98908f086c8d122fddb9
Wallace <wallacelim97@gmail.com>
2020-04-24 03:01:18 +0800

    Refactor Launch data into fragment

--- start/client/src/pages/launch.tsx
+++ start/client/src/pages/launch.tsx
@@ -6,23 +6,17 @@ import { Loading, Header, LaunchDetail } from "../components";
 import { ActionButton } from "../containers";
 import { RouteComponentProps } from "@reach/router";
 import * as LaunchDetailsTypes from "./__generated__/LaunchDetails";
+import { LAUNCH_TILE_DATA } from "./launches";
 
 export const GET_LAUNCH_DETAILS = gql`
     query LaunchDetails($launchId: ID!) {
         launch(id: $launchId) {
-            id
             site
-            isBooked
             rocket {
-                id
-                name
                 type
             }
-            mission {
-                name
-                missionPatch
-            }
-        }
+            ...LaunchTile
+        } ${LAUNCH_TILE_DATA}
     }
 `;
 
