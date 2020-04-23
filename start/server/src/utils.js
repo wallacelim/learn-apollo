321dff16b8ce743e2541cc052e50c0b820941b5c
Wallace <wallacelim97@gmail.com>
2020-04-24 00:22:56 +0800

    Hook up data sources to apollo server

--- start/server/src/utils.js
+++ start/server/src/utils.js
@@ -1,69 +1,69 @@
-const SQL = require('sequelize');
+const SQL = require("sequelize");
 
 module.exports.paginateResults = ({
-  after: cursor,
-  pageSize = 20,
-  results,
-  // can pass in a function to calculate an item's cursor
-  getCursor = () => null,
+    after: cursor,
+    pageSize = 20,
+    results,
+    // can pass in a function to calculate an item's cursor
+    getCursor = () => null,
 }) => {
-  if (pageSize < 1) return [];
+    if (pageSize < 1) return [];
 
-  if (!cursor) return results.slice(0, pageSize);
-  const cursorIndex = results.findIndex(item => {
-    // if an item has a `cursor` on it, use that, otherwise try to generate one
-    let itemCursor = item.cursor ? item.cursor : getCursor(item);
+    if (!cursor) return results.slice(0, pageSize);
+    const cursorIndex = results.findIndex((item) => {
+        // if an item has a `cursor` on it, use that, otherwise try to generate one
+        let itemCursor = item.cursor ? item.cursor : getCursor(item);
 
-    // if there's still not a cursor, return false by default
-    return itemCursor ? cursor === itemCursor : false;
-  });
+        // if there's still not a cursor, return false by default
+        return itemCursor ? cursor === itemCursor : false;
+    });
 
-  return cursorIndex >= 0
-    ? cursorIndex === results.length - 1 // don't let us overflow
-      ? []
-      : results.slice(
-          cursorIndex + 1,
-          Math.min(results.length, cursorIndex + 1 + pageSize),
-        )
-    : results.slice(0, pageSize);
+    return cursorIndex >= 0
+        ? cursorIndex === results.length - 1 // don't let us overflow
+            ? []
+            : results.slice(
+                  cursorIndex + 1,
+                  Math.min(results.length, cursorIndex + 1 + pageSize)
+              )
+        : results.slice(0, pageSize);
 };
 
 module.exports.createStore = () => {
-  const Op = SQL.Op;
-  const operatorsAliases = {
-    $in: Op.in,
-  };
+    const Op = SQL.Op;
+    const operatorsAliases = {
+        $in: Op.in,
+    };
 
-  const db = new SQL('database', 'username', 'password', {
-    dialect: 'sqlite',
-    storage: './store.sqlite',
-    operatorsAliases,
-    logging: false,
-  });
+    const db = new SQL("database", "username", "password", {
+        dialect: "sqlite",
+        storage: "./store.sqlite",
+        operatorsAliases,
+        logging: false,
+    });
 
-  const users = db.define('user', {
-    id: {
-      type: SQL.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    createdAt: SQL.DATE,
-    updatedAt: SQL.DATE,
-    email: SQL.STRING,
-    token: SQL.STRING,
-  });
+    const users = db.define("user", {
+        id: {
+            type: SQL.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        createdAt: SQL.DATE,
+        updatedAt: SQL.DATE,
+        email: SQL.STRING,
+        token: SQL.STRING,
+    });
 
-  const trips = db.define('trip', {
-    id: {
-      type: SQL.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    createdAt: SQL.DATE,
-    updatedAt: SQL.DATE,
-    launchId: SQL.INTEGER,
-    userId: SQL.INTEGER,
-  });
+    const trips = db.define("trip", {
+        id: {
+            type: SQL.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        createdAt: SQL.DATE,
+        updatedAt: SQL.DATE,
+        launchId: SQL.INTEGER,
+        userId: SQL.INTEGER,
+    });
 
-  return { users, trips };
+    return { users, trips };
 };
