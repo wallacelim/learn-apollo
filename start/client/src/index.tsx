cb0dc0c4abc46d8ae9cf710eebda96299de57f26
Wallace <wallacelim97@gmail.com>
2020-04-25 02:26:41 +0800

    Implement local queries in index.tsx and cart.tsx

--- start/client/src/index.tsx
+++ start/client/src/index.tsx
@@ -1,13 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
 import { ApolloClient } from "apollo-client";
 import { InMemoryCache, NormalizedCacheObject } from "apollo-cache-inmemory";
 import { HttpLink } from "apollo-link-http";
-import gql from "graphql-tag";
 import { ApolloProvider } from "@apollo/react-hooks";
-import React from "react";
-import ReactDOM from "react-dom";
+import gql from "graphql-tag";
+
 import Pages from "./pages";
 import injectStyles from "./styles";
 import { resolvers, typeDefs } from "./resolvers";
+
+const IS_LOGGED_IN = gql`
+    query IsUserLoggedIn {
+        isLoggedIn @client
+    }
+`;
 
 const cache = new InMemoryCache();
 
@@ -45,10 +52,15 @@ cache.writeData({
     },
 });
 
+function IsLoggedIn() {
+    const { data } = useQuery(IS_LOGGED_IN);
+    return data.isLoggedIn ? <Pages /> : <Login />;
+}
+
 injectStyles();
 ReactDOM.render(
     <ApolloProvider client={client}>
-        <Pages />
+        <IsLoggedIn />
     </ApolloProvider>,
     document.getElementById("root")
 );
